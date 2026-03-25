6252492604ece1bf7f05e5026a703c8eb917434d
vruzicka830 <vinceruzicka@gmail.com>
2026-03-24 19:16:16 -0600

    Add completed compute_factorial function

--- day1.py
+++ day1.py
@@ -1,10 +1,38 @@
-my_list = [1,2,3,4]
-for number in my_list:
-    print(number)
+import math
 
 def multiply_by_two(x):
 	return x * 2
 
-x=int(input())
+def compute_factorial(n):
+	if type(n) is not int:
+		raise TypeError("The input must be a nonnegative integer.")
+	if n < 0:
+		raise TypeError("The input must be a nonnegative integer.") # This should be a ValueError, since it's technically 
+# the right type.
+	result = 1
+	if n == 0: # Actually don't need this, because range(0) is an empty sequence so that the
+# for loop just exists immediately (without looping). Thus the function would just return result = 1.
+		return result
+	for i in range(n):
+		result = result * (i+1) # Just use result *= (i+1)
+	return result
+# Note that this is 8 space indent, while "industry standard" is 4 space
 
-print(multiply_by_two(x))
+def is_prime(n):
+	if type(n) is not int:
+		raise TypeError("input must be integer at least 2")
+	if n < 2:
+		raise ValueError("0,1,negative numbers aren't prime")
+	result = "there's a problem"
+	numbers = list(range(1,n+1))
+	if n%2 == 0:
+		return "not prime, it's even"
+	if n%3 == 0:
+		return "not prime, divisible by three"
+	if n%5 == 0:
+		return "not prime, divisible by five"
+	for i in range(n):
+		while 6*numbers[i]+1<math.sqrt(n):
+			if n%(6*numbers[i]+1)==0 or n%(6*numbers[i]+5)==0:
+				return "not prime"
+	return "prime"
