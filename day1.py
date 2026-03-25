02eb282ea0b12ea25f9a76488a2faa196f5352e8
vruzicka830 <vinceruzicka@gmail.com>
2026-03-25 15:43:15 -0600

    Complete day 1

--- day1.py
+++ day1.py
@@ -1,5 +1,3 @@
-import math
-
 def multiply_by_two(x):
 	return x * 2
 
@@ -26,16 +24,48 @@ def is_prime(n):
 "that's at least 2")
 	result = "not sure"
 	if n==2 or n==3 or n==5:
-		return "not prime"
-	numbers = list(range(1,n+1))
+		return "prime"
+	numbers = range(6,n,6)
 	if n%2 == 0:
 		return "not prime, it's even"
 	if n%3 == 0:
 		return "not prime, divisible by three"
 	if n%5 == 0:
 		return "not prime, divisible by five"
-	for i in range(n):
-		while 6*numbers[i]+1<math.sqrt(n):
-			if n%(6*numbers[i]+1)==0 or n%(6*numbers[i]+5)==0:
-				return "not prime"
-	return "prime"
+	for i in range(0,int(n**0.5)):
+		if n%(numbers[i]+1) ==0 and n!=(numbers[i]+1):
+			return "not prime"
+		if n%(numbers[i]+5) ==0 and n!=(numbers[i]+5):
+			return "not prime"
+		if n == numbers[i]+1 or n==numbers[i]+5:
+			return "prime"
+
+def is_prime_new(n):
+
+	if type(n) is not int:
+		raise TypeError("a prime is an integer")
+	if n < 2:
+		raise ValueError("a prime is a positive integer that's at least 2")
+
+	initial_primes=[2,3,5,7,11,13,17,19,23]
+
+	for i in initial_primes:
+		if n==i:
+			return True
+		if n%i==0:
+			return False
+
+	new_primes=[]
+	i=1
+	while 6*i-1<= int(n**0.5):
+		if 6*i+1 > max(initial_primes):
+			new_primes.append(6*i-1)
+			new_primes.append(6*i+1)
+		if n%(6*i-1)==0 or n%(6*i+1)==0:
+			return False
+		i=i+1
+
+	if not new_primes:
+		return True
+
+	return True
