7d8412c4a3a8cfc89e2b7aacdecfa869b796e08c
vince <vinceruzicka@gmail.com>
2026-03-26 09:04:22 -0600

    Complete day 2:direct sum and tensor product functions

--- day2.py
+++ day2.py
@@ -5,9 +5,18 @@ def direct_sum(A,B):
 	A_rows, A_cols = A.shape
 	B_rows, B_cols = B.shape
 
-	sum = np.zeros((A_rows+B_rows,A_cols+B_cols))
+	sum = np.zeros((A_rows+B_rows,A_cols+B_cols), dtype=complex)
 
 	sum[0:A_rows, 0:A_cols] = A
 	sum[A_rows:A_rows+B_rows, A_cols:A_cols+B_cols] = B
 
 	return sum
+
+A = np.array([[1+1j, 2],[3,4-2j]], dtype = complex)
+B = np.array([[-3+5j, -2],[3+2j,1-1j]], dtype = complex)
+
+print("Direct sum of A and B: ")
+print(direct_sum(A,B))
+
+print("Tensor product of A and B: ")
+print(np.kron(A,B))
